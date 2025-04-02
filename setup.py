1b815c8488573e4d51214813ea0311cb5674b22b
eltontanaka2821 <eltontanakamukarati@gmail.com>
2025-04-02 08:47:24 +0200

    versioning

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="rbprof",
-    version="0.1.8",
+    version="0.1.9",
     package_dir={"": "src"},  # Look for packages in the `src/` directory
     packages=find_packages(where="src"),  # Find packages in `src/`
     install_requires=[
